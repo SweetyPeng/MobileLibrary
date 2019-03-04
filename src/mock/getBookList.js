ee82ad162ee85cd1bdd9cb9eeac12f738df4e3bf
SweetyPeng <13065105989@163.com>
2019-03-04 19:27:26 +0800

    mock booklist

---
+++ src/mock/getBookList.js
@@ -0,0 +1,42 @@
+module.exports = {
+  result: 'success',
+  data: [
+    {
+      "author": [
+        "八月长安"
+      ],
+      "pubdate": "2009-12",
+      "origin_title": "玛丽苏病例报告",
+      "image": "https://img1.doubanio.com\/view\/subject\/m\/public\/s4293097.jpg",
+      "binding": "平装",
+      "images": {
+        "small": "https://img1.doubanio.com\/view\/subject\/s\/public\/s4293097.jpg",
+        "large": "https://img1.doubanio.com\/view\/subject\/l\/public\/s4293097.jpg",
+        "medium": "https://img1.doubanio.com\/view\/subject\/m\/public\/s4293097.jpg"
+      },
+      "id": "4166819",
+      "publisher": "新世界出版社",
+      "title": "你好，旧时光（上 下）",
+      "url": "https:\/\/api.douban.com\/v2\/book\/4166819",
+      "summary": "最好的青春共鸣文本！\n盛大的记忆共鸣曲，写给每个女孩的“成长之书”，带你重回“最美的时光”。\n网络原名《玛丽苏病例报告》，随书附赠精美2010年历卡！\n1部晋江网6年来唯一不谈情爱而人气爆棚、高居榜首的小说！\n1部让所有心中有爱的70、80、90后共同追捧的青春奇书！\n“玛丽苏”是一种“自以为是主角”的病，我们都是患者。感染无须惊慌，它只宣告成长的开始。这是一个小女孩的成长故事，这或许也是你的故事。她是女侠、雅典娜、月野兔、花仙子、希瑞、白娘子……她以为所有人都爱她，世界等着她拯救。却没想到，这世界无人可以拯救，她所能做的，只是长大。彼得潘终于走完了小时候，彼时的少年站在成长的尽头，回首过去，一路崎岖竟已遍地繁花盛开。",
+    },
+    {
+      "author": [
+        "[日] 石黑谦吾 撰文",
+        "秋元良平 摄影"
+      ],
+      "pubdate": "2003-1",
+      "image": "https://img1.doubanio.com\/view\/subject\/m\/public\/s1086519.jpg",
+      "images": {
+        "small": "https://img1.doubanio.com\/view\/subject\/s\/public\/s1086519.jpg",
+        "large": "https://img1.doubanio.com\/view\/subject\/l\/public\/s1086519.jpg",
+        "medium": "https://img1.doubanio.com\/view\/subject\/m\/public\/s1086519.jpg"
+      },
+      "id": "1001193",
+      "publisher": "南海出版公司",
+      "title": "再见了，可鲁",
+      "url": "https:\/\/api.douban.com\/v2\/book\/1001193",
+      "summary": "可鲁是一条聪明的、一生都在工作的狗。1986年6月25日到1998年7月20日，这是它的一生。所有和可鲁打过交道的人，都把可鲁当成一个鲜活的生命来看。它从来不是小狗，它有名字，它是人们心中的“可鲁”、“小可”、“阿可”……\n可鲁最可贵的是，它有特别诚挚的性格，坦率、随性、闲适、安然、忠诚，以一生的机敏来帮助人，情感激烈，却又十分克制、忍耐、低调。可鲁就像人们生活中时时想遇见的一个“好人”。\n可鲁的一生，被人善意地记录了下来，写成了这本书——《再见了，可鲁》。",
+    }
+  ]
+}
--- src/mock/mockConfig.js
+++ src/mock/mockConfig.js
@@ -2,5 +2,5 @@ import { domain } from 'config';
 
 // [`请求地址`]: '对应的js文件（js里模拟请求返回的json数据结构）'
 module.exports = {
-  [`${domain}/khaos/login`]: 'login'
+  [`${domain}/getbooklist`]: 'getBookList'
 };
