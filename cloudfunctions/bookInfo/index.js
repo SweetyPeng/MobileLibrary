b0f8e38a73f180c36f56d61c114bdaf23d81bdf6
SweetyPeng <13065105989@163.com>
2019-03-20 22:47:33 +0800

    api

--- cloudfunctions/bookInfo/index.js
+++ cloudfunctions/bookInfo/index.js
@@ -6,7 +6,7 @@ cloud.init()
 const rp = require('request-promise');
 const getBookInfo = (event, context) => {
   if (event.isbn) {
-    let res = rp('https://api.douban.com/v2/book/isbn/' + event.isbn).then(
+    let res = rp('http://t.yushu.im/v2/book/isbn/' + event.isbn).then(
       data => {
         return data;
       }
@@ -18,7 +18,7 @@ const getBookInfo = (event, context) => {
     return res;
   }
   if (event.id) {
-    let res = rp('https://api.douban.com/v2/book/' + event.id).then(
+    let res = rp('http://t.yushu.im/v2/book/' + event.id).then(
       data => {
         return data;
       }
--- cloudfunctions/getBookListByName/index.js
+++ cloudfunctions/getBookListByName/index.js
@@ -4,8 +4,9 @@ const cloud = require('wx-server-sdk')
 cloud.init()
 const rp = require('request-promise');
 // 通过关键字查询图书
+// api: 1、https://api.douban.com 2、http://t.yushu.im
 const getBookListByName = (event, context) => {
-  let res = rp('https://api.douban.com/v2/book/search?q=' + event.name).then(
+  let res = rp('http://t.yushu.im/v2/book/search?q=' + event.name).then(
     data => {
       return data;
     }
