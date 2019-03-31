f66502de064128cd5958e27681523a581c2c1a50
SweetyPeng <13065105989@163.com>
2019-03-31 22:13:52 +0800

    update  postBooks

--- cloudfunctions/postBook/index.js
+++ cloudfunctions/postBook/index.js
@@ -17,42 +17,59 @@ exports.main = async (event, context) => {
   }).get()
   if (promise.data.length == 0) {
     try {
+      const postTime = new Date(Date.now() - -8 * 60 * 60 * 1000).toJSON()
+        .substr(0, 16)
+        .replace(/-/g, '/')
+        .replace(/T/g, ' ');
       let bookOwners = [];
       let {
         ownerName
       } = event.owner;
       const owner = {
         ownerName: ownerName,
-        ownerID: event.userInfo.openId
+        ownerID: event.userInfo.openId,
+        ownerPostTime: postTime 
       };
       bookOwners.unshift(owner);
       return await db.collection('bookList').add({
         data: {
           bookID: event.bookID,
           bookName: event.bookName,
-          bookOwners: bookOwners
+          bookAuthor: event.bookAuthor,
+          bookPublisher: event.bookPublisher,
+          bookPubdate: event.bookPubdate,
+          bookSummary: event.bookSummary,
+          bookImages: event.bookImages,
+          bookOwners: bookOwners,
+          postTime: postTime
         }
       })
     } catch (e) {
       console.error(e)
     }
   } else {
+    const postTime = new Date(Date.now() - -8 * 60 * 60 * 1000).toJSON()
+        .substr(0, 16)
+        .replace(/-/g, '/')
+        .replace(/T/g, ' ');
     let {
       ownerName
     } = event.owner;
-    const owner = {
+    const newowner = {
       ownerName: ownerName,
-      ownerID: event.userInfo.openId
+      ownerID: event.userInfo.openId,
+      ownerPostTime: postTime
     };
     const resp = await db.collection('bookList').doc(event.bookId).get();
     const bookOwners = resp.data.bookOwners || [];
-    bookOwners.unshift(owner);
+    bookOwners.unshift(newowner);
     console.log('bookOwners===', bookOwners);
     return await db.collection('bookList').where({
       bookID: event.bookID
     }).update({
       data: {
-        bookOwners: bookOwners
+        bookOwners: bookOwners,
+        postTime: postTime
       }
     })
   }
